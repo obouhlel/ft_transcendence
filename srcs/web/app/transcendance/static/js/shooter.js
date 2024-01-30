16e30e1c0778dea147c68fcfe2d9d057f2f00224
Mathieu HOYER <mathieuhoyer@gmail.com>
2024-01-30 19:44:19 +0100

    move

--- srcs/web/app/transcendance/static/js/shooter.js
+++ srcs/web/app/transcendance/static/js/shooter.js
@@ -3,21 +3,52 @@ import { PointerLockControls } from 'three/addons/controls/PointerLockControls.j
 
 import { doTextGeo } from './textFont.js';
 
-const map = [1, 1, 1, 1, 1, 1, 1, 1,
-			 1, 0, 0, 0, 0, 0, 0, 1,
-			 1, 0, 0, 0, 0, 0, 0, 1,
-			 1, 0, 0, 0, 1, 0, 0, 1,
-			 1, 0, 0, 1, 0, 0, 0, 1,
-			 1, 0, 0, 0, 0, 0, 0, 1,
-			 1, 0, 0, 0, 0, 0, 0, 1,
-			 1, 0, 0, 0, 0, 0, 0, 1,
-			 1, 1, 1, 1, 1, 1, 1, 1,];
+// 1 => wall
+// 0 => space
+// 3, 4 => spaws
+const map = [
+	[1, 1, 1, 1, 1, 1, 1, 1],
+	[1, 3, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 1, 0, 0, 1],
+	[1, 0, 0, 1, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 0, 0, 0, 0, 0, 4, 1],
+	[1, 0, 0, 0, 0, 0, 0, 1],
+	[1, 1, 1, 1, 1, 1, 1, 1]
+];
+const sizeBox = 5;
+const widthMap = map[0].length * sizeBox;
+const heightMap = map.length *sizeBox;
+
+function blitMap(scene) {
+	const floor = new THREE.Mesh( new THREE.BoxGeometry( widthMap, 0.1, heightMap ), new THREE.MeshStandardMaterial( { color: 0xffffff } ) );
+	const bloc = new THREE.Mesh( new THREE.BoxGeometry( sizeBox, sizeBox, sizeBox ), new THREE.MeshStandardMaterial( { color: 0xffffff } ) );
+	floor.receiveShadow = true;
+	floor.castShadow = true;
+	bloc.receiveShadow = true;
+	bloc.castShadow = true;
+
+
+	floor.position.x += widthMap / 2;
+	floor.position.z += heightMap / 2;
+	scene.add(floor);
+	for (let z = 0; z < map.length; z++) {
+		for (let x = 0; x < map[z].length; x++) {
+			if (map[z][x] == 1) {
+				let blocCpy = bloc.clone();
+				blocCpy.position.set((x * sizeBox) + sizeBox/2, sizeBox/2, (z * sizeBox) + sizeBox/2);
+				scene.add(blocCpy);
+			}
+		}
+	}
+}
 
 export function shooter() {
 	class Player {
 		constructor(name, x, z) {
 			this.name = name;
-			this.speed = 0.1;
+			this.speed = 0.2;
 			this.pos = new THREE.Vector3(x, 0, z);
 			this.dir = new THREE.Vector3(0, 0, 0);
 			this.hp = 150;
@@ -26,15 +57,31 @@ export function shooter() {
 		}
 
 		move(keys) {
+			let vecMove = new THREE.Vector3();
 			if (keys['w']) {
-				this.speed = 0.1;
-				if (keys['shift']) {
-					this.speed = 0.2;
-				}
+				let front = new THREE.Vector3();
+				front = this.dir.normalize().multiplyScalar(this.speed);
+				vecMove.add(front);
 			}
 			if (keys['s']) {
-				this.speed = -0.1;
+				let back = new THREE.Vector3();
+				back = this.dir.normalize().multiplyScalar(-this.speed);
+				vecMove.add(back);
 			}
+			if (keys['a']) {
+				let left = new THREE.Vector3();
+				left.crossVectors(new THREE.Vector3(0, 1, 0), this.dir).normalize().multiplyScalar(this.speed);
+				vecMove.add(left);
+			}
+			if (keys['d']) {
+				let right = new THREE.Vector3();
+				right.crossVectors(this.dir, new THREE.Vector3(0, 1, 0)).normalize().multiplyScalar(this.speed);
+				vecMove.add(right);
+			}
+			vecMove.y = 0;
+			console.log(vecMove);
+			vecMove.normalize().multiplyScalar(this.speed);
+			this.pos.add(vecMove);
 		}
 
 		shoot(keys) {
@@ -92,14 +139,7 @@ export function shooter() {
 		controls.lock();
 	});
 
-	// Cursor
-
-
-	// Floor
-	const floor = new THREE.Mesh(new THREE.BoxGeometry(20, 0.1, 20), new THREE.MeshStandardMaterial({ color: 0xffffff }));
-	floor.receiveShadow = true;
-	floor.castShadow = true;
-	scene.add(floor);
+	blitMap(scene);
 
 	// Keys
 	let keys = {};
@@ -108,18 +148,31 @@ export function shooter() {
 
 	// Light
 	const globalLight = new THREE.DirectionalLight(0xffffff, 1);
-	globalLight.position.set(0, 20, 0);
+	globalLight.position.set(widthMap/2, 20, heightMap);
+	globalLight.target.position.set(widthMap/2, 0, heightMap/2);
+	scene.add(globalLight.target);
 
 	globalLight.castShadow = true;
 
-	globalLight.shadow.camera.left = -(20 / 2);
-	globalLight.shadow.camera.right = 20 / 2;
-	globalLight.shadow.camera.top = 10;
-	globalLight.shadow.camera.bottom = -10;
+	globalLight.shadow.camera.left = -(widthMap / 2);
+	globalLight.shadow.camera.right = widthMap / 2;
+	globalLight.shadow.camera.top = heightMap/2;
+	globalLight.shadow.camera.bottom = -heightMap/2;
 	globalLight.shadow.camera.near = 0.5;
 	globalLight.shadow.camera.far = 500;
 
-	scene.add(globalLight);
+	let help = new THREE.DirectionalLightHelper(globalLight);
+	scene.add(help);
+	const leftLight = globalLight.clone();
+	const rightLight = globalLight.clone();
+	const topLight = globalLight.clone();
+	leftLight.position.set(widthMap, 20, heightMap/2);
+	rightLight.position.set(0, 20, heightMap/2);
+	topLight.position.set(widthMap/2, 20, 0);
+	scene.add(topLight);
+	scene.add(leftLight);
+	scene.add(rightLight);
+ 	scene.add(globalLight);
 
 	// Loop
 	function animate() {
@@ -131,11 +184,6 @@ export function shooter() {
 		camera.position.z = player.pos.z;
 		camera.getWorldDirection(player.dir);
 
-		player.dir.y = 0;
-		player.dir.normalize();
-		player.dir.multiplyScalar(player.speed);
-		player.pos.add(player.dir);
-		player.speed = 0;
 		renderer.render(scene, camera);
 	}
 
