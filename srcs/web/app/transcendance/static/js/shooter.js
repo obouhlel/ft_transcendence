1f53c5bac0dcb7d6daa29f6eac38b39d9bf52a80
Mathieu HOYER <mathieuhoyer@gmail.com>
2024-01-30 12:19:06 +0100

    movemnt start

--- srcs/web/app/transcendance/static/js/shooter.js
+++ srcs/web/app/transcendance/static/js/shooter.js
@@ -1,21 +1,39 @@
 import { theFont } from "./pong.js";
 import * as THREE from 'three';
+import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export function shooter() {
 	class Player {
 		constructor(name, x, y) {
 			this.name = name;
-			this.pos = THREE.Vector2(x, y);
+			this.speed = 0.1;
+			this.pos = new THREE.Vector3(x, y, 0);
+			this.dir = new THREE.Vector3(0, 0, 0);
 			this.hp = 150;
 			this.dmg = 30;
 			this.ammo = 8;
 		}
 
-		move() {
-
+		move(keys) {
+			if (keys['shift']) {
+				this.speed = 0.2;
+			}
+			if (keys['w']) {
+				let tmpDir = new THREE.Vector3(this.dir.x, this.dir.y, 0);
+				tmpDir.normalize();
+				tmpDir.multiplyScalar(this.speed);
+				this.pos.add(tmpDir);
+			}
+			if (keys['s']) {
+				let tmpDir = new THREE.Vector3(this.dir.x, this.dir.y, 0);
+				tmpDir.normalize();
+				tmpDir.multiplyScalar(this.speed);
+				this.pos.sub(tmpDir);
+			}
 		}
 
-		shoot() {
+		shoot(keys) {
 
 		}
 
@@ -30,18 +48,16 @@ export function shooter() {
 	const main = document.querySelector("main");
 
 	const container = document.createElement("div");
-	container.id = "pong";
+	container.id = "shooter";
 	main.appendChild(container);
 
 	const button = document.createElement("button");
-	button.id = "buttonPong3D";
+	button.id = "buttonShooter3D";
 	button.innerHTML = "PLAY";
 	container.appendChild(button);
 	button.addEventListener("click", () => {
 		going = true;
 		button.style.display = "none";
-		playerLeft.score = 0;
-		playerRight.score = 0;
 	});
 
 	// Get the header and footer rect box
@@ -55,16 +71,72 @@ export function shooter() {
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 	container.appendChild(renderer.domElement);
 
+	let player = new Player("test", 0, 0);
+
 	// Camera
 	const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-	camera.position.y = -10;
-	camera.position.z = 20 - 5;
-	camera.rotation.x = 0.55;
+	camera.position.x = player.pos.x;
+	camera.position.y = player.pos.y;
+	camera.position.z = player.pos.z + 2;
+	camera.rotation.x = 0;
+	camera.rotation.y = 0;
+	camera.rotation.z = 0;
+
+	// Floor
+	const floor = new THREE.Mesh(new THREE.BoxGeometry(20, 20, 0.1), new THREE.MeshStandardMaterial({ color: 0xffffff }));
+	floor.position.z = 0;
+	floor.receiveShadow = true;
+	floor.castShadow = true;
+	scene.add(floor);
+
+	// Keys
+	let keys = {};
+	document.addEventListener('keydown', (e) => keys[e.key] = true);
+	document.addEventListener('keyup', (e) => keys[e.key] = false);
+
+	const globalLight = new THREE.DirectionalLight(0xffffff, 1);
+	// Setup the position of both light
+	globalLight.position.set(0, 0, 20);
+
+	// Enable shadow casting
+	globalLight.castShadow = true;
+
+	// Setup the light data and fov
+	globalLight.shadow.camera.left = -(20 / 2);
+	globalLight.shadow.camera.right = 20 / 2;
+	globalLight.shadow.camera.top = 10;
+	globalLight.shadow.camera.bottom = -10;
+	globalLight.shadow.camera.near = 0.5;
+	globalLight.shadow.camera.far = 500;
+
+	scene.add(globalLight);
+
+	// let controls = new OrbitControls(camera, renderer.domElement);
+	// controls.enableRotate = true;
+	// controls.rotateSpeed = 1.0;
+	// controls.target.set(0, 0, 0);
+
+	function debug() {
+		if (keys['+'])
+			camera.rotation.x += 0.1;
+		if (keys['-'])
+			camera.rotation.x -= 0.1;
+		if (keys['ArrowRight'])
+			camera.rotation.y -= 0.1;
+		if (keys['ArrowLeft'])
+			camera.rotation.y += 0.1;
+	}
 
 	function animate() {
 		requestAnimationFrame(animate);
 
-
+		debug();
+		player.move(keys);
+		camera.position.x = player.pos.x;
+		camera.position.y = player.pos.y;
+		camera.position.z = player.pos.z + 2;
+		camera.getWorldDirection(player.dir);
+		// controls.update();
 
 		renderer.render(scene, camera);
 	}
