d38822ad54927c5c5d917001766c9c61ad1d1485
thi-le <thi-le@student.42.fr>
2024-02-04 18:04:32 +0100

    modified:   srcs/web/app/transcendence/models.py

--- srcs/web/app/transcendence/models.py
+++ srcs/web/app/transcendence/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+	id = models.AutoField(primary_key=True)
+	login = models.CharField(max_length=30, unique=True)
+	email = models.EmailField(max_length=254, unique=True)
+	password = models.CharField(max_length=128)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)	
+	is_admin = models.BooleanField(default=False)
+	sex = models.CharField(max_length=1, default='N')
+	age = models.IntegerField(default=0)
+	token = models.CharField(max_length=128, unique=True)
+	avatar = models.CharField(max_length=128, default='/var/www/static/default_avatar.png')
+	created_at = models.DateTimeField(auto_now_add=True)
+	last_login = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return self.login
