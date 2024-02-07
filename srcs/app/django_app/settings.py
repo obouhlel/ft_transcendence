f659f3817c69f5bfd1c27ad6778e0a1ea9484bb4
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-07 16:42:37 +0100

    Update requirement.txt

--- srcs/app/django_app/settings.py
+++ srcs/app/django_app/settings.py
@@ -31,16 +31,16 @@ ALLOWED_HOSTS = ["www.transcendance.42.fr", "transcendance.42.fr", "localhost", 
 # Application definition
 
 INSTALLED_APPS = [
-	'channels',
+	'daphne',
 	'transcendence',
     'corsheaders',
+    'sslserver',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'sslserver',
 ]
 
 MIDDLEWARE = [
