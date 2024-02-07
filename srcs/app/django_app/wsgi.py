f2e94a30f31e28e13f951da634e3dd8e96dea70f
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-07 16:28:08 +0100

    Comment all the WSGI file

--- srcs/app/django_app/wsgi.py
+++ srcs/app/django_app/wsgi.py
@@ -7,19 +7,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-import os
+# import os
 
-from django.core.wsgi import get_wsgi_application
+# from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
 
 # In wsgi.py
-django_app = get_wsgi_application()
+# django_app = get_wsgi_application()
 
 
-def https_app(environ, start_response):
-    environ["wsgi.url_scheme"] = "https"
-    return django_app(environ, start_response)
+# def https_app(environ, start_response):
+#     environ["wsgi.url_scheme"] = "https"
+#     return django_app(environ, start_response)
 
 
-application = https_app
+# application = https_app
