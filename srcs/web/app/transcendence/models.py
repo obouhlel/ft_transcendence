0eb8217a066f14bcd06c8d32733432b85ebed87b
thi-le <thi-le@student.42.fr>
2024-02-07 17:04:12 +0100

    models

--- srcs/web/app/transcendence/models.py
+++ srcs/web/app/transcendence/models.py
@@ -3,48 +3,83 @@ from django.utils.translation import gettext as _
 from django.db import models
 from django.utils import timezone
 
+
+# abstractUser has the following fields:
+# username, first_name, last_name, email
+# password  groups  user_permissions is_staff is_active
+# is_superuser last_login date_joined
+# You can add more fields to the user model by creating a custom model that inherits from AbstractUser. 
+
 class CustomUser(AbstractUser):
-    # Ajoutez vos champs personnalisés ici
-    sexe = models.CharField(max_length=50)
-    birthdate = models.DateField()
-    token = models.CharField(max_length=50, null=True)
-    avatar = models.CharField(max_length=255, default='/var/www/default_avatar.webp')
-    status = models.CharField(max_length=50, default='offline')
-    id_list_friend = models.ForeignKey('ListFriends', on_delete=models.CASCADE, null=True)
+	password = models.CharField(max_length=128)
+	first_name = models.CharField(max_length=30)
+	last_name = models.CharField(max_length=30)	
+	is_admin = models.BooleanField(default=False)
+	sex = models.CharField(max_length=1, default='N')
+	age = models.IntegerField(default=0)
+	token = models.CharField(max_length=128)
+	avatar = models.CharField(max_length=128, default='/var/www/static/default_avatar.webp')
+	created_at = models.DateTimeField(auto_now_add=True)
+	last_connexion = models.DateTimeField(auto_now=True)
+	list_friends = models.ManyToManyField('CustomUser', related_name='friends')
+	list_blocked = models.ManyToManyField('CustomUser', related_name='blocked')
+	list_request = models.ManyToManyField('friend_request', related_name='request')
+	list_request_sent = models.ManyToManyField('Friend_request', related_name='request_sent')
+	stat = models.ManyToManyField('Stat_User_by_Game', related_name='stat')
+	def __str__(self):
+		return self.username
 
 class Game(models.Model):
-    name = models.CharField(max_length=50)
-    image = models.CharField(max_length=255, default='/var/www/default_game.webp')
-    description = models.CharField(max_length=1024)
-    genre = models.CharField(max_length=50, null=True)
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=30, unique=True)
+	description = models.TextField()
+	image = models.CharField(max_length=128, default='/var/www/static/default_game.webp')
+	genre = models.CharField(max_length=30)
+	created_at = models.DateTimeField(auto_now_add=True)
+	# updated_at = models.DateTimeField(auto_now=True)
+	stat = models.ForeignKey('Stat_Game', on_delete=models.CASCADE)
+	def __str__(self):
+		return self.name
 
-class StatsGame(models.Model):
-    nb_player = models.IntegerField()
-    time_played = models.IntegerField()
-    nb_party = models.IntegerField()
-    id_game = models.ForeignKey(Game, on_delete=models.CASCADE)
+class Stat_Game(models.Model):
+	id = models.AutoField(primary_key=True)
+	nb_played = models.IntegerField(default=0)
+	time_played = models.IntegerField(default=0)
+	nb_party = models.IntegerField(default=0)
+
 
 class Party(models.Model):
-    name = models.CharField(max_length=50)
-    is_full = models.BooleanField(default=False)
-    time_start = models.DateTimeField()
-    time_end = models.DateTimeField()
-    status = models.CharField(max_length=50, default='waiting')
-    score = models.IntegerField()
-    id_game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    id_user1 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user1')
-    id_user2 = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user2')
-    id_winner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='winner')
-    id_looser = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='looser')
+	id = models.AutoField(primary_key=True)
+	id_game = models.ForeignKey('Game', on_delete=models.CASCADE)
+	name = models.CharField(max_length=30, default='Party')
+	is_full = models.BooleanField(default=False)
+	started_at = models.DateTimeField(auto_now_add=True)
+	ended_at = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=30, default='Waiting')
+	player1 = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='player1')
+	player2 = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='player2')
+	id_winner = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='winner')
+	id_loser = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='loser')
+	def __str__(self):
+		return self.id
 
-class ListFriends(models.Model):
-    id_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='user')
-    id_friend = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='friend')
+class Friend_request(models.Model):
+	id = models.AutoField(primary_key=True)
+	id_sender = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='sender')
+	id_receiver = models.ForeignKey('CustomUser', on_delete=models.CASCADE, related_name='receiver')
+	created_at = models.DateTimeField(auto_now_add=True)
+	status = models.CharField(max_length=30, default='Waiting')
+	def __str__(self):
+		return self.id
 
-class StatsUserByGame(models.Model):
-    id_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    id_game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    time_played = models.IntegerField(default=0)
-    wins = models.IntegerField(default=0)
-    looses = models.IntegerField(default=0)
-    nb_played = models.IntegerField(default=0)
+
+class Stat_User_by_Game(models.Model):
+	id = models.AutoField(primary_key=True)
+	id_user = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
+	id_game = models.ForeignKey('Game', on_delete=models.CASCADE)
+	nb_played = models.IntegerField(default=0)
+	time_played = models.IntegerField(default=0)
+	nb_win = models.IntegerField(default=0)
+	nb_lose = models.IntegerField(default=0)
+	def __str__(self):
+		return self.id
