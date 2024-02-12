387722d27e10c243707c20882feacc009a1b3049
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-12 12:41:46 +0100

    Clean the dockerfile and run_server.sh

--- srcs/tools/run_server.sh
+++ srcs/tools/run_server.sh
@@ -12,4 +12,3 @@ python manage.py add_default_data
 python manage.py collectstatic --noinput
 
 python manage.py runserver 0.0.0.0:8000
-# python manage.py runsslserver 0.0.0.0:8000 --certificate /etc/ssl/certs/localhost.crt --key /etc/ssl/private/localhost.key
