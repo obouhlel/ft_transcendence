685a06f643a5d5ae6765dcab0530ab1f46d15f32
mhoyer <mathieuhoyer@gmail.com>
2024-02-17 11:39:48 +0100

    maj

--- srcs/web/app/transcendence/consumersForPong.py
+++ srcs/web/app/transcendence/consumersForPong.py
@@ -9,17 +9,72 @@ playersPong = []
 isLeftAdded = False
 isBallMoving = False
 
+# -----------------------------Getters--------------------------------
+def getOtherPlayer(self):
+    if playersPong[0].username == self.username:
+        return playersPong[1]
+    elif playersPong[1].username == self.username:
+        return playersPong[0]
+
+def getPlayerLeft():
+    for player in playersPong:
+        if player.side == 'left':
+            return player
+        
+def getPlayerRight():
+    for player in playersPong:
+        if player.side == 'right':
+            return player
+
+def getScoreString(message):
+    scoreString = str(getPlayerLeft().score) + ' - ' + str(getPlayerRight().score)
+    return scoreString
+
+# -----------------------------Loop--------------------------------
+async def broadcast(message):
+    message = json.dumps(message)
+    for player in playersPong:
+        await player.send(message)
+        
+async def sendPosition():
+    playerLeft = getPlayerLeft()
+    playerRight = getPlayerRight()
+    await playerLeft.send(json.dumps({ 'game': 'player position',
+                                       'position': playerRight.position }))
+    await playerRight.send(json.dumps({ 'game': 'player position',
+                                        'position': playerLeft.position }))
+    
+async def findWinner():
+    if getPlayerLeft().score == 10:
+        message = { 'game': 'end',
+                    'score': getScoreString(),
+                    'winner': getPlayerLeft().username }
+        await broadcast(message)
+        return True
+    elif getPlayerRight().score == 10:
+        message = { 'game': 'end',
+                    'score': getScoreString(),
+                    'winner': getPlayerRight().username }
+        await broadcast(message)
+        return True
+    return False
+
 async def gamePong():
     while True:
         global isBallMoving
+        if await findWinner() == True:
+            playersPong.clear()
+            return
+        await sendPosition()
         if isBallMoving == False:
             message = { 'game': 'start ball', 
                         'direction': random.choice([-1, 1]) }
-            await playersPong[0].send(json.dumps(message))
-            await playersPong[1].send(json.dumps(message))
+            await broadcast(message)
             isBallMoving = True
-        await asyncio.sleep(1)
+        await asyncio.sleep(0.1)
 
+
+# -----------------------------Parser--------------------------------
 def createSelf(self, message):
     global isLeftAdded
     self.username = message['username']
@@ -40,29 +95,17 @@ def starting(self, message):
 
 def playerPosition(self, message):
     self.position = message['position']
-    if playersPong[0].username == message['username']:
-        return json.dumps({ 'game': 'player position',
-                            'position': playersPong[1].position })
-    elif playersPong[1].username == message['username']:
-        return json.dumps({ 'game': 'player position',
-                            'position': playersPong[0].position })
-    return json.dumps({ 'error': 'Username doesnt exist' })
-
-def updateScore(message, indPlayerScorer):
-    playersPong[indPlayerScorer].score += 1
-    messageForScorer = json.dump({ 'game': 'score',
-                                   'score': 'you' })
-    message = json.dump({ 'game': 'score',
-                          'score': 'opponent' })
-    playersPong[indPlayerScorer].send(messageForScorer)
-    return message
+    return json.dumps({ 'game': 'ok player position'})
       
-def score(message):
-    if playersPong[0].username == message['username']:
-        return updateScore(message, 1)
-    elif playersPong[1].username == message['username']:
-        return updateScore(message, 0)
-            
+def score(self):
+    getOtherPlayer(self).score += 1
+    return json.dumps({ 'game': 'ok score' })
+
+def forfait(self):
+    playerWinner = getOtherPlayer(self)
+    playerWinner.score = 10
+    return json.dumps({ 'game': 'ok forfait' })
+
 def parseMessage(self, message):
     if 'game' in message:
         if 'username' in message:
@@ -71,12 +114,13 @@ def parseMessage(self, message):
             elif message['game'] == 'player position':
                 return playerPosition(self, message)
             elif message['game'] == 'score':
-                return score(message)
-            elif message['game'] == 'end':
-                return
+                return score(self, message)
+            elif message['game'] == 'leaved':
+                return forfait(self, message)
         return json.dumps({ 'error': 'no username for game' })
     return json.dumps({ 'error': 'Invalid token' })
 
+# -----------------------------Consumer--------------------------------
 class PongConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         playersPong.append(self)
