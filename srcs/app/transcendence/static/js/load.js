acd2e8cb12d548573ab8db7bc9e3a5176129c78d
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-22 17:28:42 +0100

    Update

--- srcs/app/transcendence/static/js/load.js
+++ srcs/app/transcendence/static/js/load.js
@@ -4,54 +4,54 @@ import { handleEditProfileFormSubmit } from './profile.js';
 import { game, listenerGame } from './game.js';
 
 window.addEventListener('hashchange', function() {
-    let page = window.location.hash.substring(1);
+	let page = window.location.hash.substring(1);
 	if (!page) {
 		page = 'home';
 	}
-    showPage(page);
+	showPage(page);
 });
 
 window.addEventListener('load', function() {
-    let page = window.location.hash.substring(1);
-    if (!page) {
-        page = 'home';
-    }
-    showPage(page);
+	let page = window.location.hash.substring(1);
+	if (!page) {
+		page = 'home';
+	}
+	showPage(page);
 });
 
 function showPage(page) {
-    fetch(`/pages/${page}`)
-    .then(response => response.json())
-    .then(data => {
+	fetch(`/pages/${page}`)
+	.then(response => response.json())
+	.then(data => {
 		const page_content = document.getElementById('page');
 		console.log('page :', page);
-        console.log('data :', data);
+		console.log('data :', data);
 		console.log(page_content);
-        page_content.innerHTML = data.page;
+		page_content.innerHTML = data.page;
 		if (page === 'home')
-        {
+		{
 			handleLoginFormSubmit();
 		}
-        else if (page === 'login')
-        {
-            handleLoginFormSubmit();
-        }
-        else if (page === 'register')
-        {
-            handleRegisterFormSubmit();
-        }
-        else if (page === 'edit_profile')
-        {
-            handleEditProfileFormSubmit();
-        }
-        else if (page === 'game')
-        {
-            game();
-            listenerGame();
-        }
-        handleLogoutFormSubmit();
-    })
-    .catch(error => {
-        console.error(error);
-    });
+		else if (page === 'login')
+		{
+			handleLoginFormSubmit();
+		}
+		else if (page === 'register')
+		{
+			handleRegisterFormSubmit();
+		}
+		else if (page === 'edit_profile')
+		{
+			handleEditProfileFormSubmit();
+		}
+		else if (page === 'game')
+		{
+			game();
+			listenerGame();
+		}
+		handleLogoutFormSubmit();
+	})
+	.catch(error => {
+		console.error(error);
+	});
 }
