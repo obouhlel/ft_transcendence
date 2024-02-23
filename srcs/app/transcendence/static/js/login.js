098340d9c0afb10fd51d7f378ea8637ed04dfe63
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-23 11:47:12 +0100

    Mise à jour des formulaires de connexion et d'inscription

--- srcs/app/transcendence/static/js/login.js
+++ srcs/app/transcendence/static/js/login.js
@@ -1,11 +1,11 @@
 import { SERVER_URL, doRequest } from './fetch.js';
 
-const client_id = 'u-s4t2ud-5b9c9133859a5333ef620d8fd41e79e5ce3174c4300678ff79c6f12c88a4cf77';
-const redirectURI42 = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=https%3A%2F%2Flocalhost%3A8000%2Fapi%2Flogin42%2F&response_type=code`
-
 export function handleLoginFormSubmit() {
+	const client_id = 'u-s4t2ud-5b9c9133859a5333ef620d8fd41e79e5ce3174c4300678ff79c6f12c88a4cf77';
+	const redirectURI42 = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=https%3A%2F%2Flocalhost%3A8000%2Fapi%2Flogin42%2F&response_type=code`;
 	const form = document.getElementById('login-form');
 	const login42 = document.getElementById('login-42');
+	const sigin42 = document.getElementById('signin');
 	if (!form) { return; }
 	if (!login42) { return; }
 
@@ -24,6 +24,11 @@ export function handleLoginFormSubmit() {
 	login42.addEventListener('click', function(event) {
 		event.preventDefault();
 		window.location.href = redirectURI42;
+	});
+
+	signin.addEventListener('click', function(event) {
+		event.preventDefault();
+		window.location.href = '#register';
 	});
 }
 
--- srcs/app/transcendence/static/js/register.js
+++ srcs/app/transcendence/static/js/register.js
@@ -1,7 +1,11 @@
 import { doRequest, SERVER_URL } from './fetch.js';
 
 export function handleRegisterFormSubmit() {
+    const client_id = 'u-s4t2ud-5b9c9133859a5333ef620d8fd41e79e5ce3174c4300678ff79c6f12c88a4cf77';
+	const redirectURI42 = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=https%3A%2F%2Flocalhost%3A8000%2Fapi%2Flogin42%2F&response_type=code`;
     const form = document.getElementById('register-form');
+    const login42 = document.getElementById('login-42');
+    const login = document.getElementById('login');
     if (!form) { return; }
 
     form.addEventListener('submit', function(event) {
@@ -28,5 +32,15 @@ export function handleRegisterFormSubmit() {
             console.error('Une erreur est survenue lors de l\'inscription :', error);
             window.location.hash = 'register';
         }
+    });
+
+    login42.addEventListener('click', function(event) {
+		event.preventDefault();
+		window.location.href = redirectURI42;
+	});
+
+    login.addEventListener('click', function(event) {
+        event.preventDefault();
+        window.location.href = '#login';
     });
 };
