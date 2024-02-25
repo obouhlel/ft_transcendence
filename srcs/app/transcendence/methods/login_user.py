2fc68fc783a4e87e330a8807ce3e83e692923d8d
thi-le <thi-le@student.42.fr>
2024-02-25 17:28:56 +0100

    status update when login and log out

--- srcs/app/transcendence/methods/login_user.py
+++ srcs/app/transcendence/methods/login_user.py
@@ -11,6 +11,7 @@ def login_user(request):
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			django_login(request, user)
+			user.status = 'online'
 			return JsonResponse({'status': 'ok', 'message': 'Vous êtes maintenant connecté en tant que ' + username})
 		else:
 			return JsonResponse({'status': 'error', 'message': 'Nom d\'utilisateur ou mot de passe incorrect.'}, status=401)
--- srcs/app/transcendence/methods/logout_user.py
+++ srcs/app/transcendence/methods/logout_user.py
@@ -3,4 +3,6 @@ from django.contrib.auth import logout as django_logout
 
 def logout_user(request):
 	django_logout(request)
+	user = request.user
+	user.status = 'offline'
 	return JsonResponse({'status': 'ok', 'message': 'Vous êtes maintenant déconnecté.'})
