cadfda906f8aed39da66620037a9924c38de56d5
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-26 17:26:12 +0100

    Mise à jour du formulaire de connexion et ajout de la connexion 42

--- srcs/app/transcendence/static/js/form/login.js
+++ srcs/app/transcendence/static/js/form/login.js
@@ -2,30 +2,33 @@ import { doRequest, SERVER_URL } from '../utils/fetch.js';
 import { callback } from '../utils/callback.js';
 
 export function handleLoginFormSubmit() {
-	const client_id = 'u-s4t2ud-b221266d69284108d856829b7bf94bddec68b82af2a4d2eb994ac4df2978deb5';
-	const redirectURI42 = `https://api.intra.42.fr/oauth/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(window.location.origin + '/api/login_42/')}&response_type=code`;
-	console.log(redirectURI42);
+	// Login form
 	const form = document.getElementById('login-form');
-	const login42 = document.getElementById('login-42');
 	if (!form) { return; }
-	if (!login42) { return; }
-
+	
 	form.addEventListener('submit', function(event) {
 		event.preventDefault();
-
+		
 		const username = document.getElementById('username').value;
 		const password = document.getElementById('password').value;
 		const data = {
 			'username': username,
 			'password': password
 		};
-
+		
 		console.log(data);
 		doRequest.post(`${SERVER_URL}/api/login/`, data, callback.login);
 	});
 
+	// 42 login
+	const url_42 = 'https://api.intra.42.fr/oauth/authorize';
+	const client_id = 'u-s4t2ud-b221266d69284108d856829b7bf94bddec68b82af2a4d2eb994ac4df2978deb5';
+	const redirect_uri = encodeURIComponent(window.location.origin + '/api/login_42/');
+	const url = `${url_42}?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code`;
+	const login42 = document.getElementById('login-42');
+	if (!login42) { return; }
 	login42.addEventListener('click', function(event) {
 		event.preventDefault();
-		window.location.href = redirectURI42;
+		window.location.href = url;
 	});
 }
