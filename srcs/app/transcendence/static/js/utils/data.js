c8102a35cdeaa7c6198b9d0dafcb8f7d677a4cf7
obouhlel <oussama_bouhlel@outlook.fr>
2024-02-27 10:33:11 +0100

    Mise à jour de la fonction dataForm dans data.js

--- srcs/app/transcendence/static/js/utils/data.js
+++ srcs/app/transcendence/static/js/utils/data.js
@@ -5,18 +5,7 @@ export function dataForm(fields)
 	fields.forEach(field => {
 		let element = document.getElementById(field);
 		if (element) {
-			let value;
-			if (field === 'avatar') {
-				const file = element.files[0];
-				if (file.size > 1000000) {
-					console.log('Image size exceeds the limit (maximal size: 1MB)');
-					return null;
-				}
-				value = file;
-			}
-			else {
-				value = element.value;
-			}
+			let value = field === 'avatar' ? element.files[0] : element.value;
 			data.append(field, value);
 		}
 		else {
