913eae04124266e4e79e4fb6515a67eaa4eb6b01
obouhlel <oussama_bouhlel@outlook.fr>
2024-03-06 09:51:39 +0100

    Refactor friends.js and tabs.js***

--- srcs/app/transcendence/static/js/profile/friends.js
+++ srcs/app/transcendence/static/js/profile/friends.js
@@ -121,3 +121,27 @@ export async function addFriendHandler()
         });
 	});
 }
+
+/*OPEN MODAL TO SEARCH AND ADD FRIENDS*/
+export function openModal()
+{
+	const modal = document.querySelector('.modal');
+	const overlay = document.querySelector('.overlay');
+	const btnCloseModal = document.querySelector('.close-modal');
+	const btnOpenModal = document.querySelector('.show-modal');
+
+	const openModal = function () {
+		document.getElementById('search').value = '';
+		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
+	};
+
+	const closeModal = function () {
+		modal.classList.add('hidden');
+		overlay.classList.add('hidden');
+	};
+
+	btnOpenModal.addEventListener('click', openModal);
+	btnCloseModal.addEventListener('click', closeModal);
+	overlay.addEventListener('click', closeModal);
+}
