0dc54d02f674ed8f276e955345b3b38d0baf2f16
obouhlel <oussama_bouhlel@outlook.fr>
2024-03-18 18:20:07 +0100

    Fix bug avatar register 42

--- srcs/app/transcendence/methods/register_42.py
+++ srcs/app/transcendence/methods/register_42.py
@@ -21,49 +21,31 @@ def register_42(request):
 	data_list = [username, password, email, firstname, lastname]
 	for data in data_list:
 		if len(data) > 50:
-			return JsonResponse(
-				{'status': 'error', 'message': 'Data is too long.'},
-				status=400
-			)
+			return JsonResponse({'status': 'error', 'message': 'Data is too long.'}, status=400)
+
 	if CustomUser.objects.filter(username=username).exists():
-		return JsonResponse(
-			{'status': 'error', 'message': 'This username is already taken.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'This username is already taken.'}, status=400)
+
 	if CustomUser.objects.filter(email=email).exists():
-		return JsonResponse(
-			{'status': 'error', 'message': 'This email is already taken.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'This email is already taken.'}, status=400)
+
 	if password != password_confirm:
-		return JsonResponse(
-			{'status': 'error', 'message': 'Passwords do not match.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'Passwords do not match.'}, status=400)
+
 	if len(password) < 8:
-		return JsonResponse(
-			{'status': 'error', 'message': 'Password must be at least 8 characters long.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'Password must be at least 8 characters long.'},	status=400)
+
 	try:
 		birthdate = timezone.datetime.strptime(birthdate, '%Y-%m-%d').replace(tzinfo=pytz.UTC)
 	except ValueError:
-		return JsonResponse(
-			{'status': 'error', 'message': 'Invalid birthdate.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'Invalid birthdate.'}, status=400)
+
 	if birthdate > timezone.now():
-		return JsonResponse(
-			{'status': 'error', 'message': 'Birthdate is in the future.'},
-			status=400
-		)
+		return JsonResponse({'status': 'error', 'message': 'Birthdate is in the future.'}, status=400)
+
+	avatar = None
 	if 'avatar' in request.FILES:
 		avatar = request.FILES['avatar']
-		if avatar.size > 1024 * 1024:
-			return JsonResponse(
-				{'status': 'error', 'message': 'Image is too large. (Max 1MB). User not created.'},
-				status=400
-			)
 
 	user = CustomUser.objects.create(
 		username=username,
@@ -73,11 +55,10 @@ def register_42(request):
 		last_name=lastname,
 		sexe=sexe,
 		birthdate=birthdate,
-		date_joined=timezone.now(),
 		avatar=avatar,
-		token=request.session.get('token'),
+		date_joined=timezone.now(),
+		token=request.session.get('token')
 	)
-	# delete this if many games or many users
 	for game in Game.objects.all():
 		Stat_User_by_Game.objects.create(user=user, game=game)
 	request.session.pop('token')
