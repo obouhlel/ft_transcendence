de1fe7ec1cb7b3f5c709dd93bc161a424acde232
thi-le <thi-le@student.42.fr>
2024-03-22 13:03:29 +0100

    change in Party.py

--- srcs/app/transcendence/models/Party.py
+++ srcs/app/transcendence/models/Party.py
@@ -62,7 +62,6 @@ class Party(models.Model):
 			'winner_party': self.winner_party.id if self.winner_party else None,
 			'loser_party': self.loser_party.id if self.loser_party else None,
 			'type': self.type,
-			'round_nb': self.round_nb,
 			'id_tournament': self.id_tournament.id if self.id_tournament else None
 	}
 	def startParty(player1, player2, game, type):
