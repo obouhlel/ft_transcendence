28d9be62e848ef0d83ff618c11cbb534d3244eb8
thi-le <thi-le@student.42.fr>
2024-03-24 00:08:24 +0100

    redirect user if refresh

--- srcs/app/transcendence/static/js/profile/stats.js
+++ srcs/app/transcendence/static/js/profile/stats.js
@@ -68,10 +68,6 @@ export async function show_dynamic_history(gameID) {
         }
 
         const adversary = data_adversary.user;
-        const data_userConnected = await doRequest.get(
-          "/api/get_user_connected"
-        );
-        const userConnected = data_userConnected.user;
         const date = new Date(party.ended_at).toLocaleDateString();
 
         html += `
