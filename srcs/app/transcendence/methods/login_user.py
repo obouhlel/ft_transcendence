cd271210eebc32ed7f5e09b870eef1c05822124b
obouhlel <oussama_bouhlel@outlook.fr>
2024-03-26 09:22:34 +0100

    Fix username validation in login_user and register_42

--- srcs/app/transcendence/methods/login_user.py
+++ srcs/app/transcendence/methods/login_user.py
@@ -8,14 +8,14 @@ import re
 def login_user(request):
 	data = json.loads(request.body)
 	username = data['username']
-	if re.match(r'^[a-zA-Z0-9_]{3,20}$', username) is None:
-		return JsonResponse({'status': 'error', 'message': 'Username must be between 3 and 20 characters long and can only contain letters, numbers and underscores.'}, status=400)
 	if not username:
 		return JsonResponse({'status': 'error', 'message': 'Username is required.'}, status=400)
 	password = data['password']
 	if not password:
 		return JsonResponse({'status': 'error', 'message': 'Password is required.'}, status=400)
 
+	if re.match(r'^[a-zA-Z0-9_]{3,20}$', username) is None:
+		return JsonResponse({'status': 'error', 'message': 'Username must be between 3 and 20 characters long and can only contain letters, numbers and underscores.'}, status=400)
 	user = authenticate(request, username=username, password=password)
 	if user is not None and user.status == 'Online':
 		return JsonResponse({'status': 'error', 'message': 'User is already logged in.'}, status=400)
