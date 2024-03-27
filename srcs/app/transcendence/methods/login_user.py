4ca9bf29826cee759a9fec41db4d18d9e472fc84
thi-le <thi-le@student.42.fr>
2024-03-27 01:50:44 +0100

    fix avatar pb

--- srcs/app/transcendence/methods/login_user.py
+++ srcs/app/transcendence/methods/login_user.py
@@ -13,16 +13,12 @@ def login_user(request):
 	password = data['password']
 	if not password:
 		return JsonResponse({'status': 'error', 'message': 'Password is required.'}, status=400)
-<<<<<<< HEAD
-	
-=======
 
 	if re.match(r'^[a-zA-Z0-9_]{3,20}$', username) is None:
 		return JsonResponse({'status': 'error', 'message': 'Username must be between 3 and 20 characters long and can only contain letters, numbers and underscores.'}, status=400)
->>>>>>> 42-Networks
 	user = authenticate(request, username=username, password=password)
-	if user is not None and user.status == 'Online':
-		return JsonResponse({'status': 'error', 'message': 'User is already logged in.'}, status=400)
+	# if user is not None and user.status == 'Online':
+	# 	return JsonResponse({'status': 'error', 'message': 'User is already logged in.'}, status=400)
 	if user is not None:
 		django_login(request, user)
 		return JsonResponse({'status': 'ok', 'message': 'You are now logged in as ' + username})
